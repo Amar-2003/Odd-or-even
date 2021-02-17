b260c91bf238fed6f6a8da126e668ee2331a13f1
Amarnath K <amarnathk5674@gmail.com>
2021-02-17 13:28:42 +0530

    bool toss implemented and checks

--- main.py
+++ main.py
@@ -6,20 +6,45 @@ with open('instructions.txt','r') as file:
 #toss
 print("Time for Toss")
 choice = input("Enter even or odd in small letter:")
-tossnumber = int(input("Enter a number for toss:"))
-rantoss = int(random.randint(0,1)
-if((tossnumber + rantoss)%2 == 0):
-    if(choice = "even"):
+if(choice != "even" and choice != "odd"):
+    print("Enter either odd or even in small!!")
+    exit(1)
+
+try:
+    tossnumber = int(input("Enter a number for toss:"))
+except:
+    print("Enter an integer!!")
+    exit(1)
+rantoss = int(random.randint(0,1))
+if (tossnumber+rantoss%2 == 0):
+    if(choice == "even"):
         print("You won the toss")
-        #To do
+        userwon = True
     else:
         print("Computer won the toss")
-        #to do
+        userwon = False
 
 else:
     if(choice == "odd"):
         print("You won the toss")
-        #To do
+        userwon = True
     else:
         print("Computer won the toss")    
-        #To do
+        userwon = False
+
+if(userwon):
+	inp = input("Enter your choice as bowl or bat in small:")
+    
+	if(inp == "bat"):
+		userbat = True
+	else:
+		userbat = False
+else:
+	if(random.randint(0,1)):
+		userbat = True
+	else:
+		userbat = False
+if(userbat):
+	print("You now take the role of batsmen")
+else:
+	print("You now take the role of Bowler")
