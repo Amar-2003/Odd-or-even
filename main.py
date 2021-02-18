3cb301e48d7dc389f93cde32f8228eb5833d9d2a
Amarnath K <amarnathk5674@gmail.com>
2021-02-18 13:21:30 +0530

    Basic structure completed

--- main.py
+++ main.py
@@ -49,8 +49,9 @@ if(userbat):
 	print("You now take the role of Batsmen")
 else:
 	print("You now take the role of Bowler")
-score1 = 0
-score2 = 0
+score1 = 0 #user's score
+score2 = 0 # computer's score
+print("Play 1 started, All the Best :)")
 #play1
 while(True):
     try:
@@ -58,7 +59,7 @@ while(True):
     except:
         print("Enter an integer between 0 and 6")
     n = int(random.randint(0,6))
-    print(n)
+    print(f"Computer chose {n}")
     if(userbat):
         if(r == n):
             print("You are out :(",end="   ")
@@ -75,4 +76,43 @@ while(True):
         else:
             score2 += n
             print(f"computer's run now is {score2}")
+#play 2
+print("Play 2 started")
+while(True):
+    try:
+        r = int(input("Enter Your run in range of 0 to 6 inclusive:"))
+    except:
+        print("Enter an integer between 0 and 6")
+    n = int(random.randint(0,6))
+    print(f"Computer chose {n}")
+    print(n)
+    if not (userbat):
+        if(r == n):
+            print("You are out :(",end="   ")
+            print(f"Your run now is {score1}")
+            break
+        else:
+            score1 += r
+            print(f"Your run now is {score1}",end="  ")
+            if(score1 > score2):
+                break
+            print(f"Runs for you to win is {score2 - score1} ")
+            
+    else:
+        if(r == n):
+            print("Computer is out :),Nice bowling",end="   ")
+            print(f"Computer's run now is {score2}")
+            break
+        else:
+            score2 += n
+            print(f"computer's run now is {score2}", end="  ")
+            if(score2 > score1):
+                break
+            print(f"runs remaining for computer to win is{score1 - score2}")
+
+if(score1 > score2):
+        print(f"You won by {score1 - score2} runs")
+else:
+        print(f"Computer won by {score2 - score1} runs ")
+
     
